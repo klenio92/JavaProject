286106c370601ddd6a939fac06b0cb9167aca74f
User <User@DESKTOP-5EUGTCV>
2023-08-16 18:47:15 -0300

    k2 Project

---
+++ JavaProject/src/k2/JavaRun/K2.java
@@ -0,0 +1,11 @@
+package k2.JavaRun;
+
+public class K2 {
+	
+	public static void main(String[] args) {
+		
+		/*tirando projeto da minha cabeca*/
+		
+	}
+
+}
---
+++ JavaProject/src/k2/classes/PaymentSlip.java
@@ -0,0 +1,5 @@
+package k2.classes;
+
+public class PaymentSlip {
+
+}
